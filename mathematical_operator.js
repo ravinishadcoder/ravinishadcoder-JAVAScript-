c67c5c89f12f5cdfe973cff89069d6517a822cdb
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-02-01 23:50:23 +0530

    This is file of basic use of mathematical operator. Day 01

---
+++ mathematical_operator.js
@@ -0,0 +1,9 @@
+// Mathematical Operator
+var quantity_headphones = 3;
+var price_of_headphones = 1000;
+
+var quantity_watch = 4;
+var price_of_watch = 800;
+
+var total_price = quantity_headphones*price_of_headphones + quantity_watch * price_of_watch;
+console.log(total_price);
